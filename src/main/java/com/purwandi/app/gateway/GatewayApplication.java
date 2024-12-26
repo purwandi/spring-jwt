2e3f1fb3b17ccf72cd315c650941aeb6be92b8d3
purwandi <free6300@gmail.com>
2024-12-26 12:19:37 +0700

    lint

--- src/main/java/com/purwandi/app/gateway/GatewayApplication.java
+++ src/main/java/com/purwandi/app/gateway/GatewayApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-	    SpringApplication app = new SpringApplication(GatewayApplication.class);
-		app.setBannerMode(Banner.Mode.OFF);
-		app.run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(GatewayApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
 
 }
